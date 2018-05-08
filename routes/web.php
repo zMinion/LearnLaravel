6ee6989e82eee650c0efc8341048cf22d72e9ccf
Andrei <pentru.orice.prostie@gmail.com>
2018-05-08 09:36:56 +0300

    User system
    Learn how to create login/register/forget/reset user system

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/verify-user/{code}', 'Auth\RegisterController@activateUser')->name('activate.user');
